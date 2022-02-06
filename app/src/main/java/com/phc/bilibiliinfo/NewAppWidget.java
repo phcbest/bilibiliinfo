cf07f2e637e12aa7ef1c66e011e0122196310748
PengHaiChen <phcbest2017@outlook.com>
2022-02-06 17:45:59 +0800

    添加桌面小部件和编写粉丝数动态更新

--- app/src/main/java/com/phc/bilibiliinfo/NewAppWidget.java
+++ app/src/main/java/com/phc/bilibiliinfo/NewAppWidget.java
@@ -13,7 +13,7 @@ public class NewAppWidget extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        CharSequence widgetText = "你好世界";
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
         views.setTextViewText(R.id.appwidget_text, widgetText);
