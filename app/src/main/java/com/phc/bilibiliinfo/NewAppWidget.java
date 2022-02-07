b2e7301e048048e44069c2461d2d053ac7a07a74
PengHaiChen <phcbest2017@outlook.com>
2022-02-07 13:19:37 +0800

    小部件添加点击事件

--- app/src/main/java/com/phc/bilibiliinfo/NewAppWidget.java
+++ app/src/main/java/com/phc/bilibiliinfo/NewAppWidget.java
@@ -1,15 +1,21 @@
 package com.phc.bilibiliinfo;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.widget.RemoteViews;
+
+import androidx.annotation.RequiresApi;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class NewAppWidget extends AppWidgetProvider {
 
+    @RequiresApi(api = Build.VERSION_CODES.Q)
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
@@ -18,10 +24,17 @@ public class NewAppWidget extends AppWidgetProvider {
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
         views.setTextViewText(R.id.appwidget_text, widgetText);
 
+        //点击后跳转到intent
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent Pintent = PendingIntent
+                .getActivity(context, 0, intent, 0);
+        views.setOnClickPendingIntent(R.id.appwidget_text, Pintent);
+
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.Q)
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
